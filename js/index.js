8d8b3bb720b67b3f5dc8379043d52109e7465a11
Persephone B <Persephonet220@gmail.com>
2021-10-23 10:25:30 -0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(".workItem1").mouseenter(function(){
+    $('#item1Pic').css('display', 'initial');
+});
+
+$('.workItem1').mouseleave(function(){
+    $("#item1Pic").css('display', 'none');
+});
+
+$(".workItem2").mouseenter(function(){
+    $('#item2Pic').css('display', 'initial');
+});
+
+$('.workItem2').mouseleave(function(){
+    $("#item2Pic").css('display', 'none');
+});
+
+$(".workItem3").mouseenter(function(){
+    $('#item3Pic').css('display', 'initial');
+});
+
+$('.workItem3').mouseleave(function(){
+    $("#item3Pic").css('display', 'none');
+});
+
